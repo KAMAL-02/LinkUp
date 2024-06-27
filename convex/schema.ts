8f0d626493252995c5a2e53e915596e333c1931c
KAMAL-02 <Kamalnayan403@gmail.com>
2024-06-27 10:34:09 +0530

    getMyConversation added

--- convex/schema.ts
+++ convex/schema.ts
@@ -17,4 +17,11 @@ export default defineSchema({
 		groupImage: v.optional(v.string()),
 		admin: v.optional(v.id("users")),
 	}),
+
+	messages: defineTable({
+		conversation: v.id("conversations"),
+		sender: v.string(), //it could be id but we using string so that it doesnt throw error during AI integration
+		content: v.string(),
+		messageType: v.union(v.literal("text"), v.literal("image"), v.literal("video")),
+	}).index("by_conversation", ["conversation"]),
 });
