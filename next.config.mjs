f25d421c0b5eae1ded5548f274c4b665ad50f76c
KAMAL-02 <Kamalnayan403@gmail.com>
2024-07-21 12:53:24 +0530

    send image and send video mutation added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: "valiant-partridge-243.convex.cloud" },
+			{ hostname: "oaidalleapiprodscus.blob.core.windows.net" },
+			{ hostname: "agreeable-puma-147.convex.cloud" } // Add this line
+		],
+	},
+};
 
 export default nextConfig;
--- src/components/home/chat-bubble.tsx
+++ src/components/home/chat-bubble.tsx
@@ -2,6 +2,10 @@ import { MessageSeenSvg } from "@/lib/svgs";
 import { IMessage, useConversationStore } from "@/store/chat-store";
 import ChatBubbleAvatar from "./chat-bubble-avatar";
 import DateIndicator from "./date-indicator";
+import Image from "next/image";
+import { useState } from "react";
+import { Dialog, DialogContent, DialogDescription } from "../ui/dialog";
+import ReactPlayer from "react-player";
 
 type chatBubbleProps = {
 	message: IMessage;
@@ -28,7 +32,23 @@ const ChatBubble = ({ me, message, previousMessage }: chatBubbleProps) => {
 	const isMember = selectedConversation?.participants.includes(message.sender?._id) || false;
 	const isGroup = selectedConversation?.isGroup;
 	const fromMe = message.sender?._id === me._id;
-	const bgClass = fromMe ? "bg-green-chat" : "bg-white dark:bg-gray-primary"
+	const bgClass = fromMe ? "bg-green-chat" : "bg-white dark:bg-gray-primary";
+
+	const [open, setOpen] = useState(false);
+
+	const renderMessageContent = () => {
+		switch (message.messageType) {
+			case "text":
+				return <TextMessage message={message} />;
+			case "image":
+				return <ImageMessage message={message} handleClick={() => setOpen(true)} />;
+			case "video":
+				return <VideoMessage message={message} />;
+			default:
+				return null;
+		}
+	};
+
 
 	if (!fromMe) {
 		return (
@@ -38,6 +58,12 @@ const ChatBubble = ({ me, message, previousMessage }: chatBubbleProps) => {
 					<ChatBubbleAvatar isGroup={isGroup} isMember={isMember} message={message} />
 					<div className={`flex flex-col z-20 max-w-fit px-2 pt-1 rounded-md shadow-md relative ${bgClass}`}>
 						<OtherMessageIndicator />
+						{renderMessageContent()}
+						{open && <ImageDialog
+						src = {message.content}
+						open={open}
+						onClose = { () => setOpen(false)}
+						/>}
 						<TextMessage message={message} />
 						<MessageTime time={time} fromMe={fromMe} />
 					</div>
@@ -53,7 +79,12 @@ const ChatBubble = ({ me, message, previousMessage }: chatBubbleProps) => {
 			<div className='flex gap-1 w-2/3 ml-auto'>
 				<div className={`flex  z-20 max-w-fit px-2 pt-1 rounded-md shadow-md ml-auto relative ${bgClass}`}>
 					<SelfMessageIndicator />
-					<TextMessage message={message} />
+					{renderMessageContent()}
+					 {open && <ImageDialog
+						src = {message.content}
+						open={open}
+						onClose = { () => setOpen(false)}
+						/>}
 					<MessageTime time={time} fromMe={fromMe} />
 				</div>
 			</div>
@@ -62,6 +93,41 @@ const ChatBubble = ({ me, message, previousMessage }: chatBubbleProps) => {
 };
 
 export default ChatBubble;
+
+const VideoMessage = ({ message }: { message: IMessage }) => {
+	return <ReactPlayer url={message.content} width='250px' height='250px' controls={true} light={true} />;
+};
+
+const ImageMessage = ({ message, handleClick}: { message: IMessage; handleClick: () => void}) => {
+	return (
+		<div className='w-[250px] h-[250px] m-2 relative'>
+			<Image
+				src={message.content}
+				fill
+				className='cursor-pointer object-cover rounded'
+				alt='image'
+				onClick={handleClick}
+			/>
+		</div>
+	);
+};
+
+const ImageDialog = ({ src, onClose, open }: { open: boolean; src: string; onClose: () => void }) => {
+	return (
+		<Dialog
+			open={open}
+			onOpenChange={(isOpen) => {
+				if (!isOpen) onClose();
+			}}
+		>
+			<DialogContent className='min-w-[750px]'>
+				<DialogDescription className='relative h-[450px] flex justify-center'>
+					<Image src={src} fill className='rounded-lg object-contain' alt='image' />
+				</DialogDescription>
+			</DialogContent>
+		</Dialog>
+	);
+};
 
 const OtherMessageIndicator = () => (
 	<div className='absolute bg-white dark:bg-gray-primary top-0 -left-[4px] w-3 h-3 rounded-bl-full' />
